ac4383a56d2f9bea4066897531dc72f433a44145
Konstantin Vasilev <vva9292@gmail.com>
2024-02-01 00:01:30 +0300

    Задание готово

---
+++ 1-map.js
@@ -0,0 +1,18 @@
+'use strict';
+const arr = [
+    { id: 1, name: 'Вася' },
+    { id: 2, name: 'Петя' },
+    { id: 1, name: 'Вася' }
+];
+
+const uniqueArray = [];
+
+arr.map(function (element) {
+    // Проверяем, есть ли уже объект с такими же свойствами в новом массиве
+    if (!uniqueArray.find(item => item.id === element.id && item.name === element.name)) {
+        uniqueArray.push(element);
+    }
+});
+
+const uniqueSet = new Set(uniqueArray);
+console.log(uniqueSet);
